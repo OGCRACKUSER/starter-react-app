f4976158e1e18eca7a2d176d9f3b89434d4a1a54
OGCRACKUSER <118628643+OGCRACKUSER@users.noreply.github.com>
2023-01-13 20:07:00 +0300

    Add files via upload

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63c18229f0019100228deeee",
+				"63c18229f0019100228deef1"
+			]
+		},
+		"63c18229f0019100228deeee": {
+			"id": "63c18229f0019100228deeee",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63c18229f0019100228deef1": {
+			"id": "63c18229f0019100228deef1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
